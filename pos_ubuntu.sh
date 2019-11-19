7d096fdf9af7c4c275eabf70750899cf2716132d
Antônio Pereira <34043361+antonio258@users.noreply.github.com>
2019-11-18 21:50:57 -0300

    Add files via upload

---
+++ pos_ubuntu.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+apt-get -y update
+apt-get -y full-upgrade
+apt-get -y install smplayer conky conky-all smplayer-themes libreoffice libreoffice-l10n-pt-br discord gimp qbittorrent rhythmbox telegram-desktop htop lm-sensors pulseeffects lsp-plugins openjdk-8-jdk openjfx ubuntu-restricted-extras
---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir -p ~/Imagens/wallpapers
+mkdir -p ~/.themes
+mkdir -p ~/.icons
+mv wallpaper ~/Imagens/wallpapers/
+mv conkyrc ~/.conkyrc
