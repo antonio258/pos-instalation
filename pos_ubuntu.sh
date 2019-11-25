325d6d33ef4e1e50dda55f3eb1604a1122fe6b02
Antônio Pereira <34043361+antonio258@users.noreply.github.com>
2019-11-25 13:38:24 -0300

    Add files via upload

--- pos_ubuntu.sh
+++ pos_ubuntu.sh
@@ -2,4 +2,4 @@
 
 apt-get -y update
 apt-get -y full-upgrade
-apt-get -y install smplayer conky conky-all smplayer-themes libreoffice libreoffice-l10n-pt-br discord gimp qbittorrent rhythmbox telegram-desktop htop lm-sensors pulseeffects lsp-plugins openjdk-8-jdk openjfx ubuntu-restricted-extras
+apt-get -y install smplayer conky conky-all smplayer-themes libreoffice libreoffice-l10n-pt-br gimp qbittorrent rhythmbox telegram-desktop htop lm-sensors pulseeffects lsp-plugins openjdk-8-jdk openjfx ubuntu-restricted-extras
