389b4a7e459ad4baedbb279756f719f15780dc94
Antônio Pereira <34043361+antonio258@users.noreply.github.com>
2019-11-25 22:28:26 -0300

    Update pos_ubuntu.sh

--- pos_ubuntu.sh
+++ pos_ubuntu.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+username=$1
 sudo apt-get -y update
 sudo apt-get -y full-upgrade
 sudo apt-get -y install smplayer conky conky-all smplayer-themes libreoffice libreoffice-l10n-pt-br gimp qbittorrent rhythmbox telegram-desktop htop lm-sensors pulseeffects lsp-plugins openjdk-8-jdk openjfx ubuntu-restricted-extras zsh wget git
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/g' .zshrc
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/g' /home/$username/.zshrc
