fe8a8c563e8f4597b9610e03ad65b2d4774653db
antonio258p <antonio258p@gmail.com>
2023-03-21 20:37:32 -0300

    update

--- config.sh
+++ config.sh
@@ -15,15 +15,14 @@ function update () {
 function install () {
     echo "installing $distro..."
     if [[ $distro = "ubuntu" ]]
-        then sudo add-apt-repository ppa:solaar-unifying/stable -y
-        sudo apt-get update
-        sudo apt-get install smplayer smplayer-themes steam wine htop persepolis obs-studio solaar default-jdk adminer zsh git wget youtube-dl libreoffice libreoffice-l10n-pt-br gimp ubuntu-restricted-extras telegram-desktop qbittorrent flatpak -y
+        then sudo apt-get update
+        sudo apt-get install smplayer smplayer-themes steam wine htop persepolis obs-studio default-jdk adminer zsh git wget youtube-dl libreoffice libreoffice-l10n-pt-br gimp ubuntu-restricted-extras telegram-desktop qbittorrent flatpak -y
     elif [[ $distro = "fedora" ]]
         then sudo dnf install smplayer smplayer-themes steam wine goverlay mangohud htop persepolis obs-studio solaar java-11-openjdk zsh git wget youtube-dl gimp telegram-desktop qbittorrent -y
             sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
     elif [[ $distro = "arch" ]]
         then sudo pacman -Syyuu -y
-        sudo pacman -S smplayer smplayer-themes steam wine htop persepolis obs-studio solaar jdk11-openjdk adminer zsh git go wget youtube-dl gimp telegram-desktop qbittorrent libreoffice-still libreoffice-still libreoffice-still-pt-br flatpak -y
+        sudo pacman -S smplayer smplayer-themes steam wine htop persepolis obs-studio solaar jdk11-openjdk zsh git go wget youtube-dl gimp telegram-desktop qbittorrent libreoffice-still libreoffice-still libreoffice-still-pt-br flatpak -y
         git clone https://aur.archlinux.org/yay.git ; cd yay ; makepkg -si
     fi
 
@@ -40,9 +39,8 @@ function extras () {
     echo "creating folders and configure fstab..."
     mkdir -p ~/.themes
     mkdir -p ~/.icons
-    sudo echo "/dev/sdb1 /mnt/HD ntfs-3g defaults,noatime,uid=1000,umask=007,x-gvfs-show 0 0" >> /etc/fstab
-    cd ~/Downloads ; git clone https://github.com/vinceliuice/Colloid-gtk-theme.git ; cd Colloid-gtk-theme ; ./install.sh --tweaks rimless -c dark -s compact --tweaks nord
-    cd ~/Downloads ; git clone https://github.com/vinceliuice/Colloid-icon-theme.git ; cd Colloid-icon-theme ; ./install.sh -s nord
+    echo "/dev/sdb1 /mnt/HD ntfs-3g defaults,noatime,uid=1000,umask=007,x-gvfs-show 0 0" | sudo tee -a /etc/fstab
+    cd ~/Downloads ; git clone https://github.com/vinceliuice/Colloid-icon-theme.git ; cd Colloid-icon-theme ; ./install.sh
     cd ~/Downloads ; wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.26.2.13244.tar.gz
     cd ~/Downloads ; wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 }
